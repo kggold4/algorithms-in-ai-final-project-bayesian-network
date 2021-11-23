85747326dd6565211aed2e915bbad1e8c42b7064
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-11-23 20:35:57 +0200

    add the cartesianTwoDoubleArrays funciton

--- src/UtilFunctions.java
+++ src/UtilFunctions.java
@@ -1,4 +1,6 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class UtilFunctions {
     /**
@@ -7,8 +9,8 @@ public class UtilFunctions {
      * @param hashmap - given hashmap
      * @return hashmap to string
      */
-    public static <K, V> String hashMapToString(HashMap<K, V> hashmap) {
-        if(hashmap.isEmpty()) return "";
+    public static <K, V> String hashMapToString(LinkedHashMap<K, V> hashmap) {
+        if (hashmap.isEmpty()) return "";
         StringBuilder output = new StringBuilder();
         hashmap.forEach((key, value) -> {
             output.append(key);
@@ -17,5 +19,34 @@ public class UtilFunctions {
             output.append("\n");
         });
         return output.toString();
+    }
+
+    /**
+     * @param keys is a key string from a CPT table, for example: "A=T,B=F,C=v1"
+     * @return hashmap when the keys are the variables name ("A", "B", "C") and the values of them are the outcomes ("T", "F", "v1")
+     */
+    public static LinkedHashMap<String, String> splitKeysToVariablesAndOutcomes(String keys) {
+        LinkedHashMap<String, String> result = new LinkedHashMap<>();
+        String[] keys_split = keys.split(",");
+        for (String key : keys_split) {
+            String[] key_split = key.split("=");
+            result.put(key_split[0], key_split[1]);
+        }
+        return result;
+    }
+
+    public static double[][] cartesianTwoDoubleArrays(double[] s1, double[] s2) {
+        List<double[]> list = new ArrayList<>();
+        for (double v1 : s1) {
+            for (double v2 : s2) {
+                list.add(new double[]{v1, v2});
+            }
+        }
+        double[][] result = new double[list.size()][2];
+        int k = 0;
+        for (double[] i : list) {
+            result[k++] = i;
+        }
+        return result;
     }
 }
